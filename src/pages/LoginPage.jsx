92c1b1a05a227ef1eeb6356634ed63b69cbcbebe
Kanykei <kanisatybaldyeva@gmail.com>
2023-09-10 10:55:56 +0600

    finish-login-page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,27 +5,57 @@ import { useNavigate } from "react-router-dom";
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
+  const [inputValid, setInputValid] = useState(false);
 
   const navigate = useNavigate();
+
+  const re = /\S+@\S+\.\S+/;
+  const onSubmit = () => {
+    if (re.test(String(email)) || pass.length >= 8) {
+      setInputValid(true);
+    } else {
+      setInputValid(false);
+      return (
+        <div>
+          <p>Не корректный пароль или емайл</p>
+        </div>
+      );
+    }
+    navigate("/main");
+  };
+
+  const handleClick = () => {
+    if (inputValid) {
+      navigate("/main");
+    } else {
+      return (
+        <div>
+          <p>Не корректный пароль или емайл</p>
+        </div>
+      );
+    }
+  };
   return (
     <div>
       <h1>Login Page</h1>
-      <label htmlFor="Email">Email</label>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Enter your email..."
-      />
-      <label htmlFor="Password"> Password</label>
-      <input
-        type="password"
-        value={pass}
-        onChange={(e) => setPass(e.target.value)}
-        placeholder="Enter your password..."
-      />
+      <form onSubmit={onSubmit}>
+        <label htmlFor="Email">Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email..."
+        />
+        <label htmlFor="Password"> Password</label>
+        <input
+          type="password"
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          placeholder="Enter your password..."
+        />
 
-      <Button onClick={() => navigate("./main")}>Submit</Button>
+        <Button onClick={() => handleClick}>Submit</Button>
+      </form>
     </div>
   );
 };
